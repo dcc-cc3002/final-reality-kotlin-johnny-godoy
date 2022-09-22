0530cd295a4978c43c1240509c673127d0b308f1
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-09-22 18:36:11 -0300

    Revert "updated Weapon string representation"
    This reverts commit bc238077c5c18f3d42609e01254da5281db962c8.

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/Weapon.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/Weapon.kt
@@ -17,6 +17,7 @@ import java.util.Objects
  * @constructor Creates a weapon with a name, a base damage, speed, and it's type.
  *
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author ~Your name~
  */
 class Weapon(
     val name: String,
@@ -37,13 +38,14 @@ class Weapon(
 
     override fun hashCode() = Objects.hash(Weapon::class, name, damage, weight, type)
 
-    override fun toString() = "Weapon(name: $name, damage: $damage, weight: $weight, type: $type)"
+    override fun toString() = "Weapon { name: $name, damage: $damage, weight: $weight, type: $type)"
 }
 
 /**
  * Enumeration of all the weapon types.
  *
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author ~Your name~
  */
 enum class WeaponType {
     SWORD, AXE, KNIFE, STAFF, BOW
--- src/main/kotlin/cl/uchile/dcc/finalreality/model/character/GameCharacter.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/GameCharacter.kt
@@ -14,6 +14,7 @@ package cl.uchile.dcc.finalreality.model.character
  *    The current health points of the character.
  *
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author ~Your name~
  */
 interface GameCharacter {
     val name: String
