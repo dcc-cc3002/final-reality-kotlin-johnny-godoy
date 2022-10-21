5fa8f750968040f2a412fa047fe760dd8ca5ea6e
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-10-21 19:56:13 -0300

    tests: add exceptions testing

---
+++ src/test/kotlin/cl/uchile/dcc/finalreality/exceptions/RequireTest.kt
@@ -0,0 +1,21 @@
+package cl.uchile.dcc.finalreality.exceptions
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class RequireTest{
+    @Test
+    fun requireTest(){
+        assertThrows(IllegalArgumentException::class.java) { require(false) }
+    }
+    @Test
+    fun atLeastTest(){
+        assertThrows(InvalidStatValueException::class.java) { Require.Stat(1, "test") atLeast 2 }
+        assertDoesNotThrow() { Require.Stat(2, "test") atLeast 1 }
+    }
+    @Test
+    fun inRangeTest(){
+        assertThrows(InvalidStatValueException::class.java) { Require.Stat(1, "test") inRange 2..3 }
+        assertDoesNotThrow() { Require.Stat(2, "test") inRange 1..3 }
+    }
+}
