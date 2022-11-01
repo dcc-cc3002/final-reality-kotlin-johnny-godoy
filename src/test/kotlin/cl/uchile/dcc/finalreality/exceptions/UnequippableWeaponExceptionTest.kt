b0480c3b44d93e44783a67698689f4daf81bb509
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-11-01 18:25:10 -0300

    tests: add engineer unequippable tests

--- src/test/kotlin/cl/uchile/dcc/finalreality/exceptions/UnequippableWeaponExceptionTest.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/exceptions/UnequippableWeaponExceptionTest.kt
@@ -1,9 +1,12 @@
 package cl.uchile.dcc.finalreality.exceptions
 
 import cl.uchile.dcc.finalreality.model.character.interfaces.IGameCharacter
+import cl.uchile.dcc.finalreality.model.character.player.Engineer
 import cl.uchile.dcc.finalreality.model.character.player.Knight
 import cl.uchile.dcc.finalreality.model.weapons.Bow
+import cl.uchile.dcc.finalreality.model.weapons.Knife
 import cl.uchile.dcc.finalreality.model.weapons.Staff
+import cl.uchile.dcc.finalreality.model.weapons.Sword
 import java.util.concurrent.LinkedBlockingDeque
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -17,5 +20,15 @@ internal class UnequippableWeaponExceptionTest{
         val bow = Bow("Bow", 10, 10)
         assertThrows(UnequippableWeaponException::class.java) { knight.equip(staff) }
         assertThrows(UnequippableWeaponException::class.java) { knight.equip(bow) }
+    }
+    @Test
+    fun testEngineer(){
+        val engineer = Engineer("Test", 5, 0, turns)
+        val staff = Staff("Staff", 10, 10, 10)
+        val sword = Sword("Sword", 10, 10)
+        val knife = Knife("Knife", 10, 10)
+        assertThrows(UnequippableWeaponException::class.java) { engineer.equip(staff) }
+        assertThrows(UnequippableWeaponException::class.java) { engineer.equip(sword) }
+        assertThrows(UnequippableWeaponException::class.java) { engineer.equip(knife) }
     }
 }
