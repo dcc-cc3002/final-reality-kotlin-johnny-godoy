0a2e8a12c73388045fb2fbc7e687b61695ea3b48
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-10-31 21:32:20 -0300

    imlement Sword column

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/weapons/Sword.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/weapons/Sword.kt
@@ -1,6 +1,10 @@
 package cl.uchile.dcc.finalreality.model.weapons
 
+import cl.uchile.dcc.finalreality.model.character.player.Knight
+import cl.uchile.dcc.finalreality.model.character.player.Thief
 import cl.uchile.dcc.finalreality.model.weapons.abstract_classes.AbstractWeapon
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.EquippableByKnight
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.EquippableByThief
 import java.util.Objects
 
 /**
@@ -17,7 +21,10 @@ import java.util.Objects
  *
  * @author <a href="https://www.github.com/johnny-godoy">Johnny</a>
  */
-class Sword(name: String, damage: Int, weight: Int) : AbstractWeapon(name, damage, weight) {
+class Sword(name: String,
+            damage: Int,
+            weight: Int
+) : AbstractWeapon(name, damage, weight), EquippableByKnight, EquippableByThief {
     override fun equals(other: Any?): Boolean = when {
         this === other -> true
         other !is Sword -> false
@@ -29,4 +36,11 @@ class Sword(name: String, damage: Int, weight: Int) : AbstractWeapon(name, damag
     }
     override fun hashCode(): Int =
         Objects.hash(Sword::class, name, damage, weight)
+
+    override fun equipToKnight(knight: Knight) {
+        knight.equip(this)
+    }
+    override fun equipToThief(thief: Thief) {
+        thief.equip(this)
+    }
 }
--- src/test/kotlin/cl/uchile/dcc/finalreality/model/weapons/SwordTest.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/model/weapons/SwordTest.kt
@@ -1,6 +1,7 @@
 package cl.uchile.dcc.finalreality.model.weapons
 
 import cl.uchile.dcc.cl.uchile.dcc.finalreality.model.weapons.abstract_classes.AbstractWeaponTest
+import cl.uchile.dcc.finalreality.model.character.player.Knight
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -13,5 +14,17 @@ internal class SwordTest : AbstractWeaponTest() {
         assertEquals("Sword(name='Test', damage=5, weight=5)", firstWeaponTest.toString())
         assertEquals("Sword(name='Test', damage=5, weight=5)", sameWeaponTest.toString())
         assertEquals("Sword(name='Different', damage=10, weight=10)", differentWeaponTest.toString())
+    }
+    @Test
+    fun equipToKnight() {
+        val knight = Knight("Test", 10, 10, turns)
+        firstWeaponTest.equipToKnight(knight)
+        assertEquals(firstWeaponTest, knight.equippedWeapon)
+    }
+    @Test
+    fun equipToEngineer() {
+        val engineer = Knight("Test", 10, 10, turns)
+        firstWeaponTest.equipToKnight(engineer)
+        assertEquals(firstWeaponTest, engineer.equippedWeapon)
     }
 }
