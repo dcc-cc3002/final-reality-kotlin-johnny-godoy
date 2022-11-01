81d01ac08b7d9626710682079c317a22e4b024a7
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-11-01 18:41:31 -0300

    refactor tests: easier testing for unequippables

--- src/test/kotlin/cl/uchile/dcc/finalreality/exceptions/UnequippableWeaponExceptionTest.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/exceptions/UnequippableWeaponExceptionTest.kt
@@ -1,9 +1,11 @@
 package cl.uchile.dcc.finalreality.exceptions
 
 import cl.uchile.dcc.finalreality.model.character.interfaces.IGameCharacter
+import cl.uchile.dcc.finalreality.model.character.player.BlackMage
 import cl.uchile.dcc.finalreality.model.character.player.Engineer
 import cl.uchile.dcc.finalreality.model.character.player.Knight
 import cl.uchile.dcc.finalreality.model.character.player.Thief
+import cl.uchile.dcc.finalreality.model.weapons.Axe
 import cl.uchile.dcc.finalreality.model.weapons.Bow
 import cl.uchile.dcc.finalreality.model.weapons.Knife
 import cl.uchile.dcc.finalreality.model.weapons.Staff
@@ -14,32 +16,28 @@ import org.junit.jupiter.api.Test
 
 internal class UnequippableWeaponExceptionTest{
     val turns: LinkedBlockingDeque<IGameCharacter> = LinkedBlockingDeque<IGameCharacter>()
+    private val axe = Axe("Axe", 10, 10)
+    private val bow = Bow("Bow", 10, 10)
+    private val knife = Knife("Knife", 10, 10)
+    private val staff = Staff("Staff", 10, 10, 10)
+    private val sword = Sword("Sword", 10, 10)
     @Test
     fun testKnight(){
-        val knight = Knight("Test", 5, 0, turns)
-        val staff = Staff("Staff", 10, 10, 10)
-        val bow = Bow("Bow", 10, 10)
-        assertThrows(UnequippableWeaponException::class.java) { knight.equip(staff) }
-        assertThrows(UnequippableWeaponException::class.java) { knight.equip(bow) }
+        val knight = Knight("Knight", 10, 10, turns)
+        assertThrows(UnequippableWeaponException::class.java, {knight.equip(staff)}, "A staff was equipped to a knight.")
+        assertThrows(UnequippableWeaponException::class.java, {knight.equip(bow)}, "A bow was equipped to a knight.")
     }
     @Test
     fun testEngineer(){
         val engineer = Engineer("Test", 5, 0, turns)
-        val staff = Staff("Staff", 10, 10, 10)
-        val sword = Sword("Sword", 10, 10)
-        val knife = Knife("Knife", 10, 10)
-        assertThrows(UnequippableWeaponException::class.java) { engineer.equip(staff) }
-        assertThrows(UnequippableWeaponException::class.java) { engineer.equip(sword) }
-        assertThrows(UnequippableWeaponException::class.java) { engineer.equip(knife) }
+        assertThrows(UnequippableWeaponException::class.java, {engineer.equip(sword)}, "A sword was equipped to an engineer.")
+        assertThrows(UnequippableWeaponException::class.java, {engineer.equip(knife)}, "A knife was equipped to an engineer.")
+        assertThrows(UnequippableWeaponException::class.java, {engineer.equip(staff)}, "A staff was equipped to an engineer.")
     }
     @Test
     fun testThief() {
         val thief = Thief("Test", 5, 0, turns)
-        val staff = Staff("Staff", 10, 10, 10)
-        val sword = Sword("Sword", 10, 10)
-        val knife = Knife("Knife", 10, 10)
+        assertThrows(UnequippableWeaponException::class.java) { thief.equip(axe) }
         assertThrows(UnequippableWeaponException::class.java) { thief.equip(staff) }
-        assertThrows(UnequippableWeaponException::class.java) { thief.equip(sword) }
-        assertThrows(UnequippableWeaponException::class.java) { thief.equip(knife) }
     }
 }
