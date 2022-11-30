f2354c62a755cdcc7b458df8b344c310af6c64fb
ClavitoB <johnny.godoy@ing.uchile.cl>
2022-11-29 23:48:59 -0300

    fix: correct equip tests

--- src/main/kotlin/cl/uchile/dcc/finalreality/controller/GameController.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/controller/GameController.kt
@@ -9,6 +9,16 @@ import cl.uchile.dcc.finalreality.model.character.player.Knight
 import cl.uchile.dcc.finalreality.model.character.player.Thief
 import cl.uchile.dcc.finalreality.model.character.player.WhiteMage
 import cl.uchile.dcc.finalreality.model.character.player.interfaces.IPlayerCharacter
+import cl.uchile.dcc.finalreality.model.weapons.Bow
+import cl.uchile.dcc.finalreality.model.weapons.Knife
+import cl.uchile.dcc.finalreality.model.weapons.Staff
+import cl.uchile.dcc.finalreality.model.weapons.Sword
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.EquippableByBlackMage
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.EquippableByEngineer
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.EquippableByKnight
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.EquippableByThief
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.EquippableByWhiteMage
+import cl.uchile.dcc.finalreality.model.weapons.interfaces.IWeapon
 
 /**
  * The controller class.
@@ -26,14 +36,14 @@ class GameController {
     val enemyCharacters: MutableList<Enemy> = mutableListOf()
     init {
         for (i in 1..5) { // Adding enemies to the game
-            enemyCharacters.add(Enemy("Enemy $i", 2*i, 10, 10, 1, this))
+            createEnemy("Enemy $i", 2*i, 10, 10)
         }
         // Adding players to the game
-        playerCharacters.add(Thief("Thief", 10, 10, this))
-        playerCharacters.add(Knight("Knight", 10, 10, this))
-        playerCharacters.add(BlackMage("Black Mage", 10, 10, 10, this))
-        playerCharacters.add(WhiteMage("White Mage", 10, 10, 10, this))
-        playerCharacters.add(Engineer("Engineer", 10, 10, this))
+        createThief("Thief", 10, 10, Sword("Knife", 10, 10))
+        createKnight("Knight", 10, 10, Knife("Knife", 10, 10))
+        createEngineer("Engineer", 10, 10, Bow("Bow", 10, 10))
+        createBlackMage("Black Mage", 10, 10, 10, Knife("Knife", 10, 10))
+        createWhiteMage("White Mage", 10, 10, 10, Staff("Staff", 10, 10, 4))
     }
     /**
      * Creates a new enemy character and adds it to the list of enemies.
@@ -45,36 +55,41 @@ class GameController {
     /**
      * Creates a new engineer character and adds it to the list of players.
      */
-    fun createEngineer(name: String, hp: Int, defense: Int) {
+    fun createEngineer(name: String, hp: Int, defense: Int, weapon: EquippableByEngineer) {
         val engineer = Engineer(name, hp, defense, this)
+        engineer.equip(weapon)
         playerCharacters.add(engineer)
     }
     /**
      * Creates a new knight character and adds it to the list of players.
      */
-    fun createKnight(name: String, hp: Int, defense: Int) {
+    fun createKnight(name: String, hp: Int, defense: Int, weapon: EquippableByKnight) {
         val knight = Knight(name, hp, defense, this)
+        knight.equip(weapon)
         playerCharacters.add(knight)
     }
     /**
      * Creates a new thief character and adds it to the list of players.
      */
-    fun createThief(name: String, hp: Int, defense: Int) {
+    fun createThief(name: String, hp: Int, defense: Int, weapon: EquippableByThief) {
         val thief = Thief(name, hp, defense, this)
+        thief.equip(weapon)
         playerCharacters.add(thief)
     }
     /**
      * Creates a new black mage character and adds it to the list of players.
      */
-    fun createBlackMage(name: String, hp: Int, defense: Int, mana: Int) {
+    fun createBlackMage(name: String, hp: Int, defense: Int, mana: Int, weapon: EquippableByBlackMage) {
         val blackMage = BlackMage(name, hp, defense, mana, this)
+        blackMage.equip(weapon)
         playerCharacters.add(blackMage)
     }
     /**
      * Creates a new white mage character and adds it to the list of players.
      */
-    fun createWhiteMage(name: String, hp: Int, defense: Int, mana: Int) {
+    fun createWhiteMage(name: String, hp: Int, defense: Int, mana: Int, weapon: EquippableByWhiteMage) {
         val whiteMage = WhiteMage(name, hp, defense, mana, this)
+        whiteMage.equip(weapon)
         playerCharacters.add(whiteMage)
     }
     /**
--- src/test/kotlin/cl/uchile/dcc/finalreality/controller/GameControllerTest.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/controller/GameControllerTest.kt
@@ -25,9 +25,9 @@ internal class GameControllerTest {
         assertEquals(playerCharacters.size, 5)
         assertEquals(playerCharacters[0].name, "Thief")
         assertEquals(playerCharacters[1].name, "Knight")
-        assertEquals(playerCharacters[2].name, "Black Mage")
-        assertEquals(playerCharacters[3].name, "White Mage")
-        assertEquals(playerCharacters[4].name, "Engineer")
+        assertEquals(playerCharacters[2].name, "Engineer")
+        assertEquals(playerCharacters[3].name, "Black Mage")
+        assertEquals(playerCharacters[4].name, "White Mage")
     }
     @Test
     fun createEnemy() {
@@ -39,36 +39,6 @@ internal class GameControllerTest {
     private fun testPlayerCreation(expectedPlayer: IGameCharacter) {
         val lastIndex = controller.playerCharacters.size - 1
         assertEquals(controller.playerCharacters[lastIndex], expectedPlayer)
-    }
-    @Test
-    fun createThief() {
-        val expectedThief = Thief("Test", 10, 10, controller)
-        controller.createThief("Test", 10, 10)
-        testPlayerCreation(expectedThief)
-    }
-    @Test
-    fun createKnight() {
-        val expectedKnight = Knight("Test", 10, 10, controller)
-        controller.createKnight("Test", 10, 10)
-        testPlayerCreation(expectedKnight)
-    }
-    @Test
-    fun createBlackMage() {
-        val expectedBlackMage = BlackMage("Test", 10, 10, 10, controller)
-        controller.createBlackMage("Test", 10, 10, 10)
-        testPlayerCreation(expectedBlackMage)
-    }
-    @Test
-    fun createWhiteMage() {
-        val expectedWhiteMage = WhiteMage("Test", 10, 10, 10, controller)
-        controller.createWhiteMage("Test", 10, 10, 10)
-        testPlayerCreation(expectedWhiteMage)
-    }
-    @Test
-    fun createEngineer() {
-        val expectedEngineer = Engineer("Test", 10, 10, controller)
-        controller.createEngineer("Test", 10, 10)
-        testPlayerCreation(expectedEngineer)
     }
     @Test
     fun attack() {
